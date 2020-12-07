f1b462af5978a13f2a645ab01f8e01be06b82bf2
Murat Han Acıpayam <acipayammurathan@gmail.com>
2020-12-07 13:10:55 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace ScadaDeneme
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btn1
+            // 
+            this.btn1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btn1.Location = new System.Drawing.Point(43, 125);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(121, 62);
+            this.btn1.TabIndex = 0;
+            this.btn1.Text = "BIR";
+            this.btn1.UseVisualStyleBackColor = true;
+            // 
+            // btn4
+            // 
+            this.btn4.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btn4.Location = new System.Drawing.Point(272, 222);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(88, 39);
+            this.btn4.TabIndex = 0;
+            this.btn4.Text = "DORT";
+            this.btn4.UseVisualStyleBackColor = true;
+            // 
+            // btn3
+            // 
+            this.btn3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btn3.Location = new System.Drawing.Point(76, 222);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(88, 39);
+            this.btn3.TabIndex = 0;
+            this.btn3.Text = "UC";
+            this.btn3.UseVisualStyleBackColor = true;
+            // 
+            // btn2
+            // 
+            this.btn2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btn2.Location = new System.Drawing.Point(272, 125);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(113, 62);
+            this.btn2.TabIndex = 0;
+            this.btn2.Text = "IKI";
+            this.btn2.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.textBox1.Location = new System.Drawing.Point(106, 36);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(212, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(428, 365);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ScadaDeneme
+{
+    public partial class Form1 : Form
+    {
+        private Size formOriginalSize;
+        private Rectangle textBox1OriginalRect;
+        private Rectangle btn1OriginalRect;
+        private Rectangle btn2OriginalRect;
+        private Rectangle btn3OriginalRect;
+        private Rectangle btn4OriginalRect;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            formOriginalSize = this.Size;
+
+            textBox1OriginalRect = new Rectangle(textBox1.Location.X, textBox1.Location.Y, textBox1.Width, textBox1.Height);
+            btn1OriginalRect = new Rectangle(btn1.Location.X, btn1.Location.Y, btn1.Width, btn1.Height);
+            btn2OriginalRect = new Rectangle(btn2.Location.X, btn2.Location.Y, btn2.Width, btn2.Height);
+            btn3OriginalRect = new Rectangle(btn3.Location.X, btn3.Location.Y, btn3.Width, btn3.Height);
+            btn4OriginalRect = new Rectangle(btn4.Location.X, btn4.Location.Y, btn4.Width, btn4.Height);
+        }
+
+        private void resizeChildrenControls()
+        {
+            resizeControl(textBox1OriginalRect, textBox1);
+            resizeControl(btn1OriginalRect, btn1);
+            resizeControl(btn2OriginalRect, btn2);
+            resizeControl(btn3OriginalRect, btn3);
+            resizeControl(btn4OriginalRect, btn4);
+
+        }
+
+        private void resizeControl(Rectangle originalControlRect,Control control)
+        {
+            float xRatio = (float)(this.Width) / (float)(formOriginalSize.Width);
+            float yRatio = (float)(this.Height) / (float)(formOriginalSize.Height);
+
+            int newX = (int)(originalControlRect.X * xRatio);
+            int newY = (int)(originalControlRect.Y * yRatio);
+            int newWidth = (int)(originalControlRect.Width * xRatio);
+            int newHeight = (int)(originalControlRect.Height * yRatio);
+
+            control.Location = new Point(newX, newY);
+            control.Size = new Size(newWidth, newHeight);
+
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            resizeChildrenControls();
+        }
+    }
+}
